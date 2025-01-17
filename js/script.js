11449314e589de7ef10f19156aca948c7f94f5a4
Pande17 <pandedwiwjy@gmail.com>
2025-01-17 16:12:15 +0800

    tambah fitur biar auto clear form

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ const button = document.getElementById('klik');
 button.addEventListener('click', function () {
 	alert('Halo, Selamat Datang');
 });
+
+function clearForm() {
+	const form = document.getElementById('dataForm');
+	const namaDepan = document.getElementById('nama_depan').value.trim();
+	const namaBelakang = document.getElementById('nama_belakang').value.trim();
+	const email = document.getElementById('email').value.trim();
+	const password = document.getElementById('password').value.trim();
+	const kota = document.getElementById('kota').value;
+
+	if (!namaDepan || !namaBelakang || !email || !password || kota === 'pilih') {
+		alert('Tidak boleh input data kosong!');
+	} else {
+		alert('Data berhasil terinput!');
+		form.reset(); // Clear the form fields
+		// namaDepan.reset();
+		// namaBelakang.reset();
+		// email.reset();
+		// password.reset();
+		// kota.reset();
+	}
+}
